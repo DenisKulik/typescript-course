b1169883f46857a26b105fc404c04a5e0bd1ec45
DenisKulik <kulikdenis1996x@gmail.com>
2024-07-20 15:29:15 +0300

    lesson 1: use types

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+// Use types
+let revenue = 1000;
+let bonus = 500;
+let c = '';
+let d = true;
+let res = revenue + bonus;
+console.log(res);
---
+++ app.ts
@@ -0,0 +1,9 @@
+// Use types
+
+let revenue: number  = 1000
+let bonus: number  = 500
+let c: string = ''
+let d: boolean = true
+
+let res: number = revenue + bonus
+console.log(res)
